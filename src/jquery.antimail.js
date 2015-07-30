46578e101606593072503cc3bfa3296a1b394bf4
Roland Kainbacher <rka@beautyparlour.at>
2015-07-30 13:33:45 +0200

    readme update

--- src/jquery.antimail.js
+++ src/jquery.antimail.js
@@ -1,37 +1,43 @@
-/*
-    written by Roland Kainbacher
-*/
-
 (function($){
-
 
     $.fn.antimail = function(options) {
 
         var mailto = '.antimail'
         var options = $.extend({
             domain: 'mydomain.com',
-            email: 'email'
+            email: 'email',
+            attribute: 'href' // 'href', 'inline', 'title'
         }, options);
 
 
         return $(mailto).each(function(){
 
-            domain = $(this).attr('data-domain');
+            domain = $(this).attr('data-antimail-domain');
             if(!domain){
                 domain = options.domain;
             };
 
-            var email = $(this).attr('data-mail');
+            var email = $(this).attr('data-antimail-email');
             if(!email){
                 email = options.email;
             };
 
-            if ( $(this).is('a')  ) {
-                var link = 'mailto:' + email + '@' + domain;
-                $(this).attr('href', link);
-            } else {
+            var attribute = $(this).attr('data-antimail-attr');
+            if(!attribute){
+                attribute = options.attribute;
+            };
+
+            if (attribute == 'inline') {
                 var link = email + '@' + domain;
                 $(this).replaceWith(link);
+            } else {
+
+                if (attribute == 'href') {
+                    var link = 'mailto:' + email + '@' + domain;
+                } else {
+                    var link = email + '@' + domain;
+                }
+                $(this).attr(attribute, link);
             }
 
         });
